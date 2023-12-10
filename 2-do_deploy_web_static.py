c691fd6f4edf121429aaef40c43d5ce3c7678bd0
root <gemgbeminiyi@gmail.com>
2023-12-10 15:33:36 -0800

    hbnb static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+distributes an archive to your web servers,
+using the function do_deploy:
+"""
+from time import strftime
+from fabric.api import env, put, run
+import os.path
+
+env.user = "ubuntu"
+env.hosts = ["54.90.37.246", "54.237.2.142"]
+
+
+def do_pack():
+    """
+    generates .tgz archive folder
+    """
+    try:
+        local("mkdir -p versions")
+        timefrmat = strftime("%Y%M%d%H%M%S")
+        filenme = "versions/web_static_{}.tgz".format(timefrmat)
+        local("tar -cvzf {} web_static/".format(filenme))
+        return filenme
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    distributes an archive to your web servers
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    try:
+        filenme = archive_path.split("/")[-1]
+        nme = filenme.split(".")[0]
+        path_r = "/data/web_static/releases/{}/".format(nme)
+        path_c = "/data/web_static/current"
+        put(archive_path, "/tmp/{}".format(filenme))
+        """put(archive_path, "/tmp/")"""
+        run("mkdir -p {}".format(path_r))
+        run("tar -xzf /tmp/{} -C {}".format(filenme, path_r))
+        run("rm /tmp/{}".format(filenme))
+        run("mv {}web_static/* {}".format(path_r, path_r))
+        run("rm -rf {}web_static".format(path_r))
+        run("rm -rf {}".format(path_c))
+        run("ln -s {} {}".format(path_r, path_c))
+        print('New version deployed!')
+        return True
+    except Exception:
+        return False
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+creates and distributes an archive to your web servers,
+using the function deploy:
+"""
+from fabric.api import env, put, run, local
+import os.path
+from time import strftime
+
+env.hosts = ["54.90.37.246", "54.237.2.142"]
+
+
+def do_pack():
+    """
+    generates .tgz archive folder
+    """
+    try:
+        local("mkdir -p versions")
+        timefrmat = strftime("%Y%M%d%H%M%S")
+        filenme = "versions/web_static_{}.tgz".format(timefrmat)
+        local("tar -cvzf {} web_static/".format(filenme))
+        return filenme
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    distributes an archive to your web servers
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    filenme = archive_path.split("/")[-1]
+    nme = filenme.split(".")[0]
+    path_r = "/data/web_static/releases/{}/".format(nme)
+    path_c = "/data/web_static/current"
+    if put(archive_path, "/tmp/{}".format(filenme)).failed is True:
+        return False
+    """put(archive_path, "/tmp/")"""
+    if run("mkdir -p {}".format(path_r)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C {}".format(filenme, path_r)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(filenme)).failed is True:
+        return False
+    if run("mv {}web_static/* {}".format(path_r, path_r)).failed is True:
+        return False
+    if run("rm -rf {}web_static".format(path_r)).failed is True:
+        return False
+    if run("rm -rf {}".format(path_c)).failed is True:
+        return False
+    if run("ln -s {} {}".format(path_r, path_c)).failed is True:
+        return False
+    print('New version deployed!')
+    return True
+
+
+def deploy():
+    """
+    deploy creates and distributes
+    """
+    arch = do_pack()
+    if arch is None:
+        return False
+    r = do_deploy(arch)
+    return r
